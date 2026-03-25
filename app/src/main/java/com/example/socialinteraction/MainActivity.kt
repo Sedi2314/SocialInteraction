fe471ec883227e22544fb266fa5bfdf318e6d37f
Sedi2314 <ST10522200@myemeris.edu.za>
2026-03-26 00:41:35 +0200

    Social Interaction debugging
    debugging code has been added

--- app/src/main/java/com/example/socialinteraction/MainActivity.kt
+++ app/src/main/java/com/example/socialinteraction/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.socialinteraction
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -50,27 +51,34 @@ not type the right day of time it will tell them to input the correct day of tim
                 val socialInput = socialInput.text.toString().lowercase().trim()
 
                 if (socialInput == "morning") {
+                    Log.d("App debug","morning executed")
                     socialOutput.setText("Good morning text to family member");
 
                 } else if (socialInput == "mid morning"){
+                    Log.d("App debug","mid morning executed")
                     socialOutput.setText("Reach out to a colleague with a quick 'Thank you' ");
                 }
                 else if(socialInput == "afternoon"){
+                    Log.d("App debug","afternoon executed")
                     socialOutput.setText("Share a funny meme or interesting link with a friend");
                 }
                 else if(socialInput == "afternoon snack time"){
-                    socialOutput.setText("Send a quick 'Thinking of you' messege");
+                    Log.d("App debug","afternoon snack time executed")
+                    socialOutput.setText("Send a quick 'Thinking of you' message");
 
                 } else if(socialInput == "dinner"){
+                    Log.d("App debug","dinner executed")
                     socialOutput.setText("Call a friend or relative for a 5 minute catchup");
                 }
                 else if(socialInput == "night"){
-                    socialOutput.setText(" Leave a thoughtful comment on a friends post");
+                    Log.d("App debug"," night executed")
+                    socialOutput.setText(" Leave a thoughtful comment on a friend's post");
 
                 }else if(socialInput == "afternoon dinner") {
+                    Log.d("App debug","afternoon dinner executed")
                     socialOutput.setText(" Leave a thoughtful comment on a friends post");
 
-                }else{
+                }else{Log.d("App debug","mInvalid input received")
                     socialOutput.setText("Please enter correct time of day")
                 }
 
